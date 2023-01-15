d456aa70743ac8ed3208d81f17413d3934bb4428
00953137 <100845140+00953137@users.noreply.github.com>
2023-01-15 17:38:42 +0800

    Update models.py

--- ACGPN_train/models/models.py
+++ ACGPN_train/models/models.py
@@ -17,6 +17,8 @@ def create_model(opt):
         print("model [%s] was created" % (model.name()))
 
     if opt.isTrain and len(opt.gpu_ids):
+        CUDA = '0,1'
+        os.environ['CUDA_VISIBLE_DEVICES'] = CUDA
         model = torch.nn.DataParallel(model, device_ids=opt.gpu_ids)
 
     return model
